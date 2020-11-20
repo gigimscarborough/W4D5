cb7a60b070b1092dbf8a2a17ed1a581c4ac3268d
David <davidjhinku@gmail.com>
2020-11-20 17:04:44 -0500

    Completed anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -8,8 +8,8 @@ def anagram?(word_1, word_2)
 
 end
 
-p anagram?("gizmo", "sally")    #=> false
-p anagram?("elvis", "lives")    #=> true
+# p anagram?("gizmo", "sally")    #=> false
+# p anagram?("elvis", "lives")    #=> true
 
 # Write a method #second_anagram? that iterates over the first string. 
 # For each letter in the first string, find the index of that letter in 
@@ -31,9 +31,41 @@ def second_anagram?(word_1, word_2)
     matching_word.empty?
 end
 
-p second_anagram?("gizmo", "sally")    #=> false
-p second_anagram?("elvis", "lives")    #=> true
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
 
+
+# Write a method #third_anagram? that solves the problem by sorting both strings alphabetically. The strings are then anagrams if and only if the sorted versions are the identical.
+
+# What is the time complexity of this solution? Is it better or worse than #second_anagram??
+
+def third_anagram?(word_1, word_2)
+    word_1.split("").sort == word_2.split("").sort
+
+    # sorted_word_1 = word_1.split("").sort
+    # sorted_word_2 = word_2.split("").sort
+
+    # sorted_word_1 == sorted_word_2
+end
+
+p third_anagram?("gizmo", "sally")    #=> false
+p third_anagram?("elvis", "lives")    #=> true
+
+
+# Write one more method #fourth_anagram?. This time, use two Hashes to store the number of times each letter appears in both words. Compare the resulting hashes.
+
+def fourth_anagram?(word_1, word_2)
+    count1 = Hash.new(0)    
+    word_1.each_char { |char| count1[char] += 1 }
+
+    count2 = Hash.new(0)    
+    word_2.each_char { |char| count2[char] += 1 }
+
+    count1 == count2
+end
+
+p fourth_anagram?("gizmo", "sally")    #=> false
+p fourth_anagram?("elvis", "lives")    #=> true
 
 # def permutations(array)
 #     return [array] if array.length <= 1
